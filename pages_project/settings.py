7f4684cb875e90f52869d7bb0566554a7739c90a
root <ppanbeing@gmail.com>
2020-10-17 03:27:38 -0400

    new updates for Heoku

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u^6)51wl53v%!3d4jue0e@lys5#_a+8j*o+jd^&ak-ds)-iv*('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
